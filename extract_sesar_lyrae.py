b869eb93fceb498a15135a35f54340cd73b31917
Dan F-M <dan@danfm.ca>
2011-06-23 22:13:37 -0400

    Batch extract sesar's lyrae

--- extract_sesar_lyrae.py
+++ extract_sesar_lyrae.py
@@ -1,87 +1,49 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Lyrae module
+
 
 History
 -------
-2011-06-22 - Created by Dan Foreman-Mackey
+2011-06-23 - Created by Dan Foreman-Mackey
 
 """
 
-__all__ = ['fit']
+import os
+import os.path
 
 import numpy as np
-from numpy.linalg import lstsq
 
-def fit(omega,time,data,order=3):
-    """
-    Fit a general RR Lyrae model to a time series
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pylab as pl
 
-    Solve A.x = F
-    
-    Parameters
-    ----------
-    time : numpy.ndarray (N,)
-        The list of time steps
-    
-    data : numpy.ndarray (N,2)
-        (flux,counts) for each time step
+from calibration import extract_calibrated_lightcurve
+import lyrae.sesar as sesar
+from lyrae import fit
 
-    Optional
-    --------
-    order : int (default : 3)
-        What order of model should we use
 
-    Returns
-    -------
-    model : function
-        A function that returns the model flux at a given time
-    
-    History
-    -------
-    2011-06-22 - Created by Dan Foreman-Mackey
-    
-    """
-    # MAGIC: construct A matrix
-    # MAGIC: 7 parameters in model
-    A = np.ones((len(time),1+2*order))
-    for i in range(order):
-        A[:,2*i+1] = np.sin((i+1)*omega*time)
-        A[:,2*i+2] = np.cos((i+1)*omega*time)
 
-    res = lstsq(A,data[:,0])[0]
-    model = lambda t: res[0]+\
-            np.sum([res[2*i+1]*np.sin((i+1)*omega*t)+\
-                    res[2*i+2]*np.cos((i+1)*omega*t) for i in range(order)],axis=0)
+for obj in sesar.coords:
+    sid = obj['Num']
+    print 'Lyrae number: ',sid
 
-    chi2 = np.sum((data[:,0]-model(time))**2/data[:,1]**2)
+    bp = os.path.join('lcs','%d'%sid)
+    if not os.path.exists(bp):
+        os.makedirs(bp)
 
-    return model, chi2
-
-if __name__ == '__main__':
-    import h5py
-    import time as timer
-    import sesar
-
-    sid = 1052471
     sesardata = sesar.table1['%d'%sid][...]
     inds = sesardata['g'] > 0
     s_time = sesardata['gmjd'][inds]
     s_data = 1e9*10**(-sesardata['g']/2.5)[inds]
 
-    f = h5py.File('testlc.hdf5')
-    data = f['data'][...]
-    time = f['time'][...]
-    f.close()
+    time,data = extract_calibrated_lightcurve(obj['ra'],obj['dec'])
 
     inds = data[:,0] > 0
     data = data[inds,:]
     time = time[inds]
     data[:,0] *= 1e9
     data[:,1] *= 1e9
-    #data[:,0] = -2.5*np.log10(data[:,0])
-    #data[:,1] = 2.5/data[:,0]/np.log(10)*data[:,1]
 
     # grid in frequency
     domega = 1.0/(time.max()-time.min())
@@ -100,10 +62,6 @@ if __name__ == '__main__':
     sortedchi2 = np.array(chi2)[inds]
 
     # plotting
-    import matplotlib
-    matplotlib.use('Agg')
-    import matplotlib.pylab as pl
-
     pl.figure(figsize=(8.,8.))
     pl.subplot(211)
     pl.plot(T,chi2,'k')
@@ -123,7 +81,7 @@ if __name__ == '__main__':
     pl.xlabel(r'$T\,[\mathrm{days}]$',fontsize=16.)
     pl.ylabel(r'$\chi^2$',fontsize=16.)
 
-    pl.savefig('chi2.png')
+    pl.savefig(os.path.join(bp,'chi2.png'))
 
     pl.figure()
     omegas = 2*np.pi/sortedT[:10]
@@ -149,6 +107,6 @@ if __name__ == '__main__':
         pl.xlabel(r'$t\%T\,[\mathrm{days}]$',fontsize=16.)
         pl.ylabel(r'$\mathrm{nMgy}$',fontsize=16.)
         pl.title(r'$T=%f\,\mathrm{days}$'%(T),fontsize=16.)
-        pl.savefig('grid/%04d.png'%i)
+        pl.savefig(os.path.join('%04d.png'%i))
 
-    
+
