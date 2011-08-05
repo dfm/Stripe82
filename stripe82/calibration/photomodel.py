fixed variance vs. inverse variance

stripe82/calibration/photomodel.py
@@ -62,15 +62,12 @@ class PhotoData:
             self.observations.append(doc)
         self.magprior = np.array([[s['g'],s['Err_g']**2] for s in self.stars])
         self.flux = data[:,:,0]
-        self.ivar = data[:,:,1]**2
+        self.ivar = data[:,:,1]
         self.nobs = len(self.obsids) #np.shape(data)[0]
         self.nstars = np.shape(data)[1]
 
     def mjd(self):
         return np.array([obs['mjd_g'] for obs in self.observations])
-
-def unpickle_photomodel(self,*args):
-    return PhotoModel(*args)
 
 class PhotoModel:
     """
@@ -99,10 +96,6 @@ class PhotoModel:
         self.conv,self.npars = self.param_names()
         self.from_vector(vector)
 
-    def __reduce__(self):
-        """For pickling"""
-        return (unpickle_photomodel,(data,vector))
-    
     def param_names(self):
         """
         Get the list of model parameters and their conversion functions
@@ -387,8 +380,8 @@ def odds_variable(p,data):
     lnpvar         = np.zeros(np.shape(data.flux))
     lnpvar[inds]   = np.logaddexp(pow3,pow2)
 
-    return np.log(p.pvar)-np.log(1-p.pvar)\
-            +lnpvar-lnpconst
+    return np.sum(np.log(p.pvar)-np.log(1-p.pvar)\
+            +lnpvar-lnpconst,axis=0)
 
 #def lnprob_badobs(p,data):
 #    """

stripe82/extract_lightcurve.py
@@ -11,20 +11,94 @@ History
 
 __all__ = ['extract_lightcurve']
 
-import pylab as pl
+import os
+import cPickle as pickle
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as pl
+
 import numpy as np
-from calibration import calibrate
+
+from calibration import calibrate,PhotoModel,odds_variable
+from lyrae import sesar,fit,find_period
 
 def extract_lightcurve(ra,dec,radius):
-    model = calibrate(ra,dec,radius=radius)
-    mjd = model.data.mjd()
-    flux = model.data.flux/model.zero[:,np.newaxis]
-    for i in range(np.shape(flux)[1]):
-        pl.clf()
-        pl.plot(mjd,flux[:,i],'+k')
-        pl.savefig('lcs/%03d.png'%i)
+    return calibrate(ra,dec,radius=radius)
 
 if __name__ == '__main__':
-    extract_lightcurve(29.47942,0.383557,10)
+    tmpfn = 'tmp.pkl'
+    ra,dec = 29.47942,0.383557 #10.0018734334081,0.791580301596976
 
+    if np.any(sesar.coords['ra'] == ra):
+        ind = sesar.coords[sesar.coords['ra'] == ra]['Num']
+        period = sesar.coords[sesar.coords['ra'] == ra]['Per']
+        sesardata = sesar.table1['%d'%(ind)][...]
+        inds = sesardata['g'] > 0
+        s_time = sesardata['gmjd'][inds]
+        s_data = 1e9*10**(-sesardata['g']/2.5)[inds]
+    else:
+        s_data = None
+        period = None
+    
+    if os.path.exists(tmpfn):
+        model = PhotoModel(*pickle.load(open(tmpfn,'rb')))
+    else:
+        model = calibrate(ra,dec,radius=10)
+        pickle.dump((model.data,model.vector()),open(tmpfn,'wb'),-1)
+    mjd = model.data.mjd()
+    if period is None:
+        period = max(mjd)+1
+    flux = model.data.flux/model.zero[:,np.newaxis]*1e9
+    varodds = odds_variable(model,model.data)
+
+    if True: # find period
+        i = 24
+        inv = model.data.ivar
+        inds = inv[:,i] > 0
+        err0_i = 1e9/np.sqrt(inv[inds,i]*model.zero[inds]**2)
+        flux_i = flux[inds,i]
+        mjd_i = mjd[inds]
+        
+        data = np.zeros([len(flux_i),2])
+        data[:,0] = flux_i
+        data[:,1] = err0_i
+        my_period = find_period(mjd_i,data)
+        print my_period,period
+        period = my_period
+    
+
+    for i in range(np.shape(flux)[1]):
+        inv = model.data.ivar
+        inds = inv[:,i] > 0
+        err0_i = 1e9/np.sqrt(inv[inds,i]*model.zero[inds]**2)
+        # learning error bars
+        #err_i  = 1/np.sqrt(inv[inds,i]*model.zero[inds]**2+model.jitterabs2 \
+        #        +model.jitterrel2*model.flux[i]**2)*(1e9)
+        flux_i = flux[inds,i]
+        mjd_i = mjd[inds]
+
+        pl.clf()
+        if s_data is not None:
+            pl.plot(s_time%period,s_data,'og',alpha=0.3)
+            pl.plot(s_time%period+period,s_data,'og',alpha=0.3)
+        
+        #pl.errorbar(mjd_i%period,flux_i,yerr=err_i,fmt='.k',alpha=0.5)
+        pl.errorbar(mjd_i%period,flux_i,yerr=err0_i,fmt='.k')
+        if s_data is not None:
+            pl.errorbar(mjd_i%period+period,flux_i,yerr=err0_i,fmt='.k')
+
+        pl.gca().axhline(model.flux[i]*1e9,color='r',ls='--')
+
+        if s_data is not None:
+            data = np.zeros([len(flux_i),2])
+            data[:,0] = flux_i
+            lyrae_fit,chi2 = fit(2*np.pi/period,mjd_i,data)
+            x = np.linspace(0,period*2,500)
+            pl.plot(x,lyrae_fit(x),'-k')
+
+        star = model.data.stars[i]
+        pl.title('(%f,%f) and %f'%(star['ra'],star['dec'],varodds[i]))
+        print i,star['ra'],star['dec'],varodds[i]
+        pl.savefig('lcs/%03d.png'%i)
 

stripe82/lyrae/lyrae.py
@@ -9,7 +9,7 @@ History
 
 """
 
-__all__ = ['fit']
+__all__ = ['fit','find_period']
 
 import numpy as np
 from numpy.linalg import lstsq
@@ -58,6 +58,45 @@ def fit(omega,time,data,order=3):
     chi2 = np.sum((data[:,0]-model(time))**2/data[:,1]**2)
 
     return model, chi2
+
+def find_period(time,data,order=3):
+    """
+    Find the period of some data
+    
+    time : numpy.ndarray (N,)
+        The list of time steps
+    
+    data : numpy.ndarray (N,2)
+        (flux,counts) for each time step
+
+    Optional
+    --------
+    order : int (default : 3)
+        What order of model should we use
+    
+    Returns
+    -------
+    period : float
+        In the same units as time
+    
+    History
+    -------
+    2011-08-05 - Created by Dan Foreman-Mackey
+    
+    """
+    # grid in frequency
+    domega = 0.1/(time.max()-time.min())
+    omegas = 2*np.pi*np.arange(1.0/1.3,1.0/0.2,domega)
+    print "omega_min,omega_max,d_omega = ",omegas.min(),omegas.max(),domega
+    print "nomega = ",len(omegas)
+    chi2 = []
+    for omega in omegas:
+        model,diff = fit(omega,time,data)
+        chi2.append(diff)
+    T = 2*np.pi/omegas
+    inds = np.argsort(chi2)
+    sortedT = T[inds]
+    return sortedT[0]
 
 if __name__ == '__main__':
     import h5py