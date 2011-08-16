Fixed overflow in logodds2prob, fixed some plots, finding some lyrae...

stripe82/calibration/opt.py
@@ -32,7 +32,13 @@ def prob2odds(prob):
     return prob / (1. - prob)
 
 def logodds2prob(logodds):
-    return odds2prob(np.exp(logodds))
+    logoddsflat = logodds.flatten()
+    prob = odds2prob(np.exp(logoddsflat))
+    # fix overflow
+    isinfornan = np.isinf(prob) + np.isnan(prob)
+    if np.any(isinfornan):
+        prob[isinfornan] = 1.0
+    return prob.reshape(np.shape(logodds))
 
 def prob2logodds(prob):
     return np.log(prob2odds(prob))

stripe82/extract_lightcurve.py
@@ -136,10 +136,9 @@ def plot_lightcurve(i,mjd,flux,err,model,ax=None,badodds=None,period=None,
     #else:
     #    title = r""
     title = r""
-    title += r"$N_\mathrm{obs} = %d"
+    title += r"$N_\mathrm{obs} = %d"%(np.sum(inds))
     if pvar is not None:
-        title += ",\,p_\mathrm{var} = %.3f$"%\
-                        (np.sum(inds),pvar[i])
+        title += ",\,\ln\,p_\mathrm{var} = %.3f$"%(np.log(pvar[i]))
     ax.set_title(title,fontsize=16)
 
     return period
@@ -200,6 +199,8 @@ if __name__ == '__main__':
         os.makedirs(bp)
     if args.tmpfile is not None:
         tmpfile = os.path.join(bp,args.tmpfile)
+    else:
+        tmpfile = None
 
     ra,dec = float(args.ra),float(args.dec)#29.47942,0.383557 #10.0018734334081,0.791580301596976
     radius = float(args.radius)
@@ -212,6 +213,8 @@ if __name__ == '__main__':
         inds = sesardata['g'] > 0
         s_time = sesardata['gmjd'][inds]
         s_data = mag2nmgy(sesardata['g'][inds])
+        #s_err  = mag2nmgy(sesardata['g'][inds]+sesardata['gerr'][inds]) - s_data #+\
+        #s_err  = s_data-mag2nmgy(sesardata['g'][inds]-sesardata['gerr'][inds])
     else:
         s_data = None
         period = None
@@ -229,12 +232,6 @@ if __name__ == '__main__':
         period = max(mjd)+1
     varodds = odds_variable(model,model.data)
     pvar = logodds2prob(varodds)
-    if np.any(np.isinf(pvar)) or np.any(np.isnan(pvar)):
-        for i in np.arange(len(varodds))[np.isinf(pvar) + np.isnan(pvar)]:
-            if varodds[i] > 0:
-                pvar[i] = 1.0
-            else:
-                pvar[i] = 0.0
 
     badodds = odds_bad(model,model.data)
     sorted_inds = np.argsort(model.flux)
@@ -250,8 +247,8 @@ if __name__ == '__main__':
                 nperiods=2,fit_period=True,pvar=pvar)
         if s_data is not None:
             for i in range(2):
-                ax.scatter(s_time%period+i*period,s_data,
-                        c=[0.5,0.5,0.5],alpha=0.5,marker='s')
+                ax.plot(s_time%period+i*period,s_data,'s',
+                        color=[0.5,0.5,0.5],alpha=0.5)
         pl.savefig(os.path.join(bp,'target.png'))
 
     if args.all or args.debug:

stripe82/find_lyrae.py
@@ -34,29 +34,31 @@ def main():
     candidates = [obj for obj in starsdb.find(
         {"pos": {"$within": {"$box": [[-29.,-0.4],[-20.,-0.2]]}},
             "lyrae_candidate": True})]
-    print len(candidates),"candidates"
     ind = np.random.randint(len(candidates))
-    for ind in range(len(candidates)):
+    for ind in np.arange(len(candidates))+308:
         print "candidate:",ind,"of",len(candidates)
         radius = 5.
         ra,dec = candidates[ind]['ra'],candidates[ind]['dec']
         strt = time.time()
 
         # extracting and calibrating
-        mjd,flux,err,model = extract_lightcurve(ra,dec,radius)
+        try:
+            mjd,flux,err,model = extract_lightcurve(ra,dec,radius)
+        except AttributeError:
+            print "extract lightcurve failed"
+        else:
+            pickle.dump((ra,dec,radius,mjd,flux,err,model.data,model.vector()),
+                    open(os.path.join(modbp,"%03d.pkl"%(ind)),"wb"),-1)
 
-        pickle.dump((ra,dec,radius,mjd,flux,err,model.data,model.vector()),
-                open(os.path.join(modbp,"%03d.pkl"%(ind)),"wb"),-1)
+            #fitting lightcurve
+            data = np.zeros([len(mjd),2])
+            data[:,0] = flux
+            data[:,1] = err
+            period, A = find_period(mjd,data,full_output=True)
+            print period
+            pickle.dump((period,A),open(os.path.join(resbp,"%03d.pkl"%(ind)),"wb"),-1)
 
-        #fitting lightcurve
-        data = np.zeros([len(mjd),2])
-        data[:,0] = flux
-        data[:,1] = err
-        period, A = find_period(mjd,data,full_output=True)
-        print period
-        pickle.dump((period,A),open(os.path.join(resbp,"%03d.pkl"%(ind)),"wb"),-1)
-
-        print "it took: ",time.time()-strt
+            print "it took: ",time.time()-strt
 
 
 if __name__ == '__main__':

stripe82/lyrae/lyrae.py
@@ -107,7 +107,11 @@ def find_period(time,data,order=3,nmin=50,N=100,full_output=False):
     minchi2 = chi2[inds[0]]
     omega_f = omegas[inds[0]]
     for i in inds[:nmin]:
-        ws = np.linspace(omegas[i-1],omegas[i+1],N)
+        try:
+            ws = np.linspace(omegas[i-1],omegas[i+1],N)
+        except:
+            dw = omegas[1]-omegas[0]
+            ws = np.linspace(omegas[i]-dw,omegas[i]+dw,N)
         c2 = []
         for w in ws:
             model,diff = fit(w,time,data)