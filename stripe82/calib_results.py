e098bf0e93af1d45c0db50d089d2ad4a96c2ea43
Dan F-M <dfm265@broiler>
2011-08-16 06:18:35 +0000

    Starting calib_results

---
+++ stripe82/calib_results.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+A couple of wrappers around the results of a calibration optimization
+
+History
+-------
+2011-08-16 - Created by Dan Foreman-Mackey
+
+"""
+
+__all__ = ['']
+
+import numpy as np
+import pylab as pl
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
