b69407ef3d8c3ba3ff3a9870bf49e6e3cfcd0755
Dan F-M <dan@danfm.ca>
2011-12-04 22:08:46 -0500

    back to dense gps and it works again

--- calibration/gp/gp.py
+++ calibration/gp/gp.py
@@ -13,10 +13,6 @@ __all__ = ['GaussianProcess']
 
 import numpy as np
 
-import scipy.sparse as sp
-# import scipy.sparse.linalg
-import scipy.linalg as linalg
-
 try:
     from numpy.linalg import slogdet
 except ImportError:
@@ -32,154 +28,115 @@ except ImportError:
         return s, np.sum(np.log(absd))
 
 import scipy.optimize as op
-
-from _gp import _sparse_k
+import scipy.linalg as linalg
 
 class GaussianProcess(object):
-    """
-    A Gaussian process object
-
-    Parameters
-    ----------
-    kernel : function
-        The kernel to use
-
-    History
-    -------
-    2011-09-07 - Created by Dan Foreman-Mackey
-
-    """
-    def __init__(self, s2=1.0, a2=1.0, la2=1.0, b2=1.0, lb2=1.0):
-        self._s2  = s2
-        self._a2  = a2
-        self._b2  = b2
-        self._la2 = la2
-        self._lb2 = lb2
-        self._L,self._alpha = None,None
+    def __init__(self, s2=1.0, a2=1.0, la2=1.0):
+        self.s2  = s2
+        self.a2  = a2
+        self.la2 = la2
+        self.L,self.alpha = None,None
 
     def __repr__(self):
         return "GaussianProcess(s2=%f,a2=%f,la2=%f,b2=%f,lb2=%f)"\
-                %(self._s2, self._a2, self._la2, self._b2, self._lb2)
+                %(self.s2, self.a2, self.la2, self.b2, self.lb2)
 
-    def K(self,x,y=None):
-        if y is None:
-            y = x
-        K = _sparse_k(self._a2, self._la2, self._b2, self._lb2, x, y,
-                s2=self._s2, grad=0)
-        b = K + self._s2 * sp.identity(len(x),format="csc")
-        return b
+    def k(self, x, y):
+        p = (x[:, None] - y[None, :])**2/self.la2
+        k = self.a2*np.exp(-0.5*p)
+        return k, p
 
-    def fit(self,x,y):
-        self._x = x
-        self._y = y
-        self._Kxx = self.K(x)
-        self._detK = None
-        self._L = sp.linalg.splu(self._Kxx)
-        self._alpha = self._L.solve(y)
+    def K(self, x, y):
+        return self.s2*np.identity(len(y))+self.k(x,y)[0]
 
-    def __call__(self,x,cov=False):
-        assert self._L is not None
-        ks = _sparse_k(self._a2, self._la2, self._b2, self._lb2, x, self._x)
-        # calculate the mean
-        f = ks.dot(np.atleast_2d(self._alpha).T)[:,0]
-        if not cov:
-            return f
-        kss = _sparse_k(self._a2, self._la2, self._b2, self._lb2, x, x)
-        kik = np.zeros(ks.shape)
-        for i in xrange(ks.shape[0]):
-            kik[i,:] = self._L.solve(np.array(ks[i,:].todense())[0])
-        v = kss - ks.dot(kik.T)
-        return f,v
+    def dK(self, x):
+        k, p = self.k(x, x)
+        dS = 2*np.sqrt(self.s2)*np.identity(len(self.y))
+        da = 2*k/np.sqrt(self.a2)
+        dl = k*p/np.sqrt(self.la2)
+        return self.s2*np.identity(len(self.y))+k, [dS, da, dl]
 
-    def optimize(self,x,y):
-        x -= np.mean(x)
-        x /= np.var(x)
-        y -= np.mean(y)
-        y /= np.var(y)
+    def condition(self, x, y):
+        self.mu_x, self.std_x = np.mean(x, axis=0), np.std(x, axis=0)
+        self.mu_y, self.std_y = np.mean(y, axis=0), np.std(y, axis=0)
+        self.x, self.y = (x-self.mu_x)/self.std_x, (y-self.mu_y)/self.std_y
+
+        self.Kxx, self.dKxx = self.dK(self.x)
+        self.L = linalg.cho_factor(self.Kxx)
+        self.alpha = linalg.cho_solve(self.L, self.y)
+        self.denom = self.y.size*np.log(2*np.pi)
+
+    def predict(self, x0):
+        x0 = (x0-self.mu_x)/self.std_x
+
+        # mean
+        k0 = self.k(self.x,x0)[0]
+        mean = np.dot(self.alpha, k0)
+
+        # cov
+        v = np.dot(linalg.cho_solve(self.L, k0).T, k0)
+        cov = self.k(x0, x0)[0] - v
+
+        return mean, cov
+
+    def sample(self, x, N=1):
+        mean,cov = self.predict(x)
+        return mean*self.std_y+self.mu_y, \
+                np.random.multivariate_normal(mean,cov,N)*self.std_y+self.mu_y
+
+    def marginal_loglike(self):
+        logp = -0.5*np.dot(self.y, self.alpha) - np.trace(self.L[0]) - self.denom
+        return logp
+
+    def grad_loglike(self):
+        # gradient
+        grad = [np.trace(np.dot(np.outer(self.alpha, self.alpha), self.dKxx[i])
+            -linalg.cho_solve(self.L, self.dKxx[i])) for i in range(3)]
+        return 0.5*np.array(grad)
+
+    def train(self, x, y):
         def chi2(p):
-            self._a2, self._b2, self._lb2, self._s2 = np.exp(p)
-            # if self._lb2 < self._la2:
-            #     return np.inf
-            # self._s2 = np.exp(p[-1])
-            self.fit(x,y)
-            s,lndet = slogdet(self._Kxx.todense())
-            detK = lndet + len(y)*np.log(2*np.pi)
-            c2 = np.dot(y,self._alpha) + detK
-            print c2
-            return c2
+            self.s2, self.a2, self.la2 = p**2
+            self.condition(x,y)
+            lnlike = self.marginal_loglike()
+            print p**2, lnlike
+            return -lnlike
 
-        # def gradchi2(p):
-        #     print "grad"
-        #     dKdtheta = self._gradK
-        #     gradc2 = []
-        #     for i in range(4):
-        #         dK = dKdtheta[i]
-        #         gradc2.append(-0.5*np.trace(\
-        #                 np.dot(np.outer(self._alpha, self._alpha), dK)\
-        #                 -self._L.solve(dK.todense())))
-        #     print gradc2
-        #     return gradc2
+        def dchi2(p):
+            self.s2, self.a2, self.la2 = p**2
+            self.condition(x,y)
+            dlnlike = self.grad_loglike()
+            # print dp, np.sum(np.abs(dp))
+            return -dlnlike
 
-        p0 = np.sqrt([self._a2, self._b2, self._lb2, self._s2])
-        # p0 = np.append(p0, np.sqrt([self._b2, self._lb2]))
-        # p0 = np.append(p0, np.log(self._s2))
-        self.fit(x,y)
-        op.fmin_bfgs(chi2,p0,disp=1)
-
-    def sample_prior(self,x):
-        """
-        Return N samples from the prior
-
-        Parameters
-        ----------
-        x : numpy.ndarray
-            Positions in parameter space
-
-        Returns
-        -------
-        ret : type
-            Description
-
-        """
-        return np.random.multivariate_normal(np.zeros(len(x)),self.K(x).todense())
-
-    def sample(self,x,N=1):
-        """
-        Sample given some data
-
-        """
-        mean,cov = self(x,cov=True)
-        return np.random.multivariate_normal(mean,cov,N)
+        p0 = np.sqrt([self.s2, self.a2, self.la2])
+        print op.fmin_bfgs(chi2,p0,fprime=dchi2,maxiter=10)**2
 
 if __name__ == '__main__':
-    import matplotlib
-    matplotlib.use('Agg')
+    # import matplotlib
+    # matplotlib.use('Agg')
     import matplotlib.pyplot as pl
     np.random.seed(5)
 
-    N = 15
+    N = 400
     s = 10
     x = np.linspace(0,s,N)#s*np.random.rand(N)
-    y = np.sin(x) + 0.05*np.random.randn(N)
+    y = np.sin(x) + 0.1*np.random.randn(N)
 
-    p = GaussianProcess(a2=0.0, la2=0.001)
-    p._s2 = 0.001
-    p.optimize(x,y)
-    p.fit(x,y)
-    print p
+    p = GaussianProcess(a2=0.05, la2=0.05)
+    p.s2 = 0.1
+    p.train(x,y)
 
     # plot fit
-    x0 = np.linspace(min(x),max(x),500)
-    y0 = p.sample(x0,100)
-    pl.plot(x0,y0.T,'.k',alpha=0.1)
-
-    mean,cov = p(x0,cov=True)
-    pl.plot(x0,mean)
+    x0 = np.linspace(min(x),max(x),100)
+    y0, samples = p.sample(x0, N=500)
+    pl.plot(x0,samples.T,'k',alpha=0.1)
+    pl.plot(x0,y0,'b')
 
     #plot data
     pl.plot(x,y,'.r')
 
-    pl.savefig("%d.png"%s)
+    pl.show()
 
     # pl.figure()
     # pl.imshow(cov)
